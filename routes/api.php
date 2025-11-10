7b85754ca3a2429f25e4ea24d0680fe738c364a5
wulan <@wulandewipramesti-dotcom>
2025-11-10 13:37:09 +0800

    menambahkan file

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php 
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PresenceApiController;
+use App\Http\Controllers\PresenceDetailApiController;
+
+Route::apiResource('presence', PresenceApiController::class);
+Route::apiResource('presence-detail', PresenceDetailApiController::class);
